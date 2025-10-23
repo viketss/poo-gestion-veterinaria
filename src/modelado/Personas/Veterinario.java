96ed8cdd5668429ef06cfe56bc0610601ddb64e2
Limon <victoriacuomo11@gmail.com>
2025-10-23 13:42:10 -0300

    feat: constructor veterinario

--- src/modelado/Personas/Veterinario.java
+++ src/modelado/Personas/Veterinario.java
@@ -2,4 +2,13 @@ public class Veterinario extends Persona { // herencia de Persona
     // atributos
     private String especialidad;
     private float sueldo;
+
+    // constructor
+    public Veterinario(String nombre, String apellido, long dni, String especialidad, float sueldo) {
+        super(nombre, apellido, dni); // llamar al constructor de la clase padre
+        this.especialidad = especialidad;
+        this.sueldo = sueldo;
+    }
+
+
 }
