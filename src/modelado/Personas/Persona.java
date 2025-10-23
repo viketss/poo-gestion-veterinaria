658b7da063730b7b5e7f6c872b980dc129c02369
Limon <victoriacuomo11@gmail.com>
2025-10-23 13:39:55 -0300

    feat: atributos, getters, setters, constructor y metodos de persona

--- src/modelado/Personas/Persona.java
+++ src/modelado/Personas/Persona.java
@@ -1,3 +1,40 @@
 public abstract class Persona {
+    // atributos
+    private String nombre;
+    private String apellido;
+    private long dni;
 
+    // constructor
+    public Persona(String nombre, String apellido, long dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    // getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public long getDni() {
+        return dni;
+    }
+    public void setDni(long dni) {
+        this.dni = dni;
+    }
+
+    // metodos
+    public void listarDatos() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("DNI: " + dni);
+    }
 }
