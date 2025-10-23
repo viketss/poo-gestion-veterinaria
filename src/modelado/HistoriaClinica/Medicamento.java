fba8a3b0b83b7bfa82399f46261fe083ab0e916a
Limon <victoriacuomo11@gmail.com>
2025-10-23 15:46:02 -0300

    feat: clase Medicamento

--- src/modelado/HistoriaClinica/Medicamento.java
+++ src/modelado/HistoriaClinica/Medicamento.java
@@ -1,3 +1,38 @@
 public class Medicamento {
+    // atributos:
+    private String nombreMedicamento;
+    private float precio;
+    private int cantidadDiasDosis;
 
+    // constructor
+    public Medicamento(String nombreMedicamento, float precio, int cantidadDiasDosis) {
+        this.nombreMedicamento = nombreMedicamento;
+        this.precio = precio;
+        this.cantidadDiasDosis = cantidadDiasDosis;
+    }
+
+    // getters y setters
+    public String getNombreMedicamento() {
+        return nombreMedicamento;
+    }
+    public void setNombreMedicamento(String nombreMedicamento) {
+        this.nombreMedicamento = nombreMedicamento;
+    }
+    public float getPrecio() {
+        return precio;
+    }
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+    public int getCantidadDiasDosis() {
+        return cantidadDiasDosis;
+    }
+    public void setCantidadDiasDosis(int cantidadDiasDosis) {
+        this.cantidadDiasDosis = cantidadDiasDosis;
+    }
+
+    // metodos:
+    public float calcularCostoPorDias() {
+        return precio * cantidadDiasDosis;
+    }
 }
