b1d7788f86e55399304ac44da841563b685d0d56
Limon <victoriacuomo11@gmail.com>
2025-10-23 13:57:37 -0300

    feat: constructor, getters, setters. metodos de cliente

--- src/modelado/Personas/Cliente.java
+++ src/modelado/Personas/Cliente.java
@@ -5,6 +5,35 @@ public class Cliente extends Persona {
     private List<Mascota> mascotas;
     private long telefono;
 
-    
+    // constructor
+    public Cliente(String nombre, String apellido, long dni, List<Mascota> mascotas, long telefono) {
+        super(nombre, apellido, dni); // llamar al constructor de la clase padre
+        this.mascotas = mascotas;
+        this.telefono = telefono;
+    }
+
+    // getters y setters
+    public List<Mascota> getMascotas() {
+        return mascotas;
+    }
+    public long getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(long telefono) {
+        this.telefono = telefono;
+    }
+
+    // metodos
+    public void agregarMascota(Mascota mascota) {
+
+    }
+
+    public void obtenerInfoDeContacto() {
+
+    }
+
+    public void solicitarTurno(Turno turno) {
+
+    }
 
 }
