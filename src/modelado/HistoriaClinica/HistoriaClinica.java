94d37fe6738c2e4f979c30fbc17b264d8f5416a4
Limon <victoriacuomo11@gmail.com>
2025-10-23 15:36:15 -0300

    feat: clase HistoriaClinica

--- src/modelado/HistoriaClinica/HistoriaClinica.java
+++ src/modelado/HistoriaClinica/HistoriaClinica.java
@@ -1,3 +1,49 @@
-public class HistoriaClinica {
+import java.util.ArrayList;
+import java.util.List;
 
-}
+public class HistoriaClinica {
+    // atributos
+    private List<Turno> historialDeTurnos;
+    private List<Tratamiento> historialDeTratamientos;
+    private int id;
+
+    // constructor
+    public HistoriaClinica(int id) {
+        this.id = id;
+        this.historialDeTurnos = new ArrayList<>();
+        this.historialDeTratamientos = new ArrayList<>();
+    }
+
+    //getters y setters
+    public List<Turno> getHistorialDeTurnos() {
+        return historialDeTurnos;
+    }
+    public void setHistorialDeTurnos(List<Turno> historialDeTurnos) {
+        this.historialDeTurnos = historialDeTurnos;
+    }
+    public List<Tratamiento> getHistorialDeTratamientos() {
+        return historialDeTratamientos;
+    }
+    public void setHistorialDeTratamientos(List<Tratamiento> historialDeTratamientos) {
+        this.historialDeTratamientos = historialDeTratamientos;
+    }
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // metodos
+    public void generarReporte() {
+
+    }
+    
+    public void agregarTurno(Turno turno) {
+
+    }
+
+    public void agregarTratamiento(Tratamiento tratamiento) {
+
+    }
+} 
