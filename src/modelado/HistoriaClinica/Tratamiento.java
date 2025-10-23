2f6a494d965d66021872e0a6cc17740d735ed3e3
Limon <victoriacuomo11@gmail.com>
2025-10-23 15:43:19 -0300

    feat: clase Tratamiento

--- src/modelado/HistoriaClinica/Tratamiento.java
+++ src/modelado/HistoriaClinica/Tratamiento.java
@@ -1,3 +1,44 @@
 public class Tratamiento {
+    // atributos:
+    private String descripcion;
+    private float costoBase;
+    private Medicamento medicamento;
+
+    // constructor
+    public Tratamiento(String descripcion, float costoBase, Medicamento medicamento) {
+        this.descripcion = descripcion;
+        this.costoBase = costoBase;
+        this.medicamento = medicamento;
+    }
+
+    // getters y setters
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    public float getCostoBase() {
+        return costoBase;
+    }
+    public void setCostoBase(float costoBase) {
+        this.costoBase = costoBase;
+    }
+    public Medicamento getMedicamento() {
+        return medicamento;
+    }
+    public void setMedicamento(Medicamento medicamento) {
+        this.medicamento = medicamento;
+    }
+
+    // metodos
+    public float calcularCostoTotal() {
+        return 0;
+    }
+
+    public void administrarMedicamento(Medicamento medicamento) {
+        
+    }
+
 
 }
