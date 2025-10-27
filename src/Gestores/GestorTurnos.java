3e84407afc6303266a645b7a7a50ec2ebd8ec007
Limon <victoriacuomo11@gmail.com>
2025-10-27 20:24:30 -0300

    feat: info basica gestor de turnos

--- src/Gestores/GestorTurnos.java
+++ src/Gestores/GestorTurnos.java
@@ -1,5 +1,17 @@
 package Gestores;
 
-public class GestorTurnos {
+import modelado.HistoriaClinica.Turno;
+
+public class GestorTurnos { // controlador que coordina la creacion de turnos
+    // patron GRASP 1 - creator (crea turno)
+    // crear/instanciar objetos de la clase Turno
+
+    // metodos
+    // reservar turno
+    public Turno solicitarTurno() {
+        return null;
+    }
+    // validar la disponibilidad del turno
+    // asignar veterinario
 
 }
