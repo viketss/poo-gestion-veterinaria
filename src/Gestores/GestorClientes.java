885c0f7053c6cb68ee0c725f4a68b682f4b8ef61
tomasflorydev <florytomas@protonmail.com>
2025-10-27 23:16:33 -0300

    feat: buscarCliente

--- src/Gestores/GestorClientes.java
+++ src/Gestores/GestorClientes.java
@@ -1,5 +1,24 @@
 package Gestores;
 
+import modelado.Personas.Cliente;
+import java.util.List;
+
 public class GestorClientes {
 
+    private List<Cliente> listaClientes;
+
+    public GestorClientes(List<Cliente> listaClientes) {
+        this.listaClientes = listaClientes;
+    }
+
+    public Cliente buscarCliente(String nombre) {
+        for (Cliente cliente : this.listaClientes) {
+            if (cliente.getNombre().equals(nombre)) {
+                System.out.println("GESTION CLIENTES: Cliente encontrado.");
+                return cliente; // Retorna el objeto Cliente completo
+            }
+        }
+        System.out.println("GESTION CLIENTES: Cliente no encontrado.");
+        return null; // Retorna null si no se encontró coincidencia
+    }
 }
