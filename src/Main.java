32bb5470b43941ed0df593ffead7cb6c0a5a1e98
Limon <victoriacuomo11@gmail.com>
2025-10-27 21:09:27 -0300

    feat: empuzo el main

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import Persistencia.GestorPersistencia;
 import modelado.Mascotas.Mascota;
 import modelado.Mascotas.TipoMascota;
+import modelado.Personas.Cliente;
 
 public class Main {
+    // instanciar los gestores
+    private static final GestorPersistencia PERSISTENCIA = new GestorPersistencia();
     public static void main(String[] args) {
+        Cliente cliente1 = new Cliente("Tomas Flory", "Gonzalez", 40456789, null, 1122334455L); // TODO falta agregr lista de mascotas
         Mascota nuevaMascota = new Mascota("Aras", "Callejero", TipoMascota.PERRO, null, true, 7);
         nuevaMascota.setVacunado(false); // prueba
     }
