e41fd4927707061fe8d2a4ef51be95f5ee58e0e8
Limon <victoriacuomo11@gmail.com>
2025-10-28 16:33:50 -0300

    muchas cosas

--- src/SOLID/S/GenerarReporteDatosClinicos.java
+++ src/SOLID/S/GenerarReporteDatosClinicos.java
@@ -2,6 +2,6 @@ package SOLID.S;
 
 public class GenerarReporteDatosClinicos {
     // principio SOLID: single responsibility 
-
+    // TODO implementar: costos, turnos, tratamientos, medicamentos
     // IDEAL guardar en archivo el dato clinico de una mascota
 }
